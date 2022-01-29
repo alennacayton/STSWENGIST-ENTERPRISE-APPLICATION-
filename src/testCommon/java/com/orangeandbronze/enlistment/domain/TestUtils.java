7487f427706f5e257b58e89b3ec84dae65e66233
alenna <rainiiicorn@gmail.com>
2022-01-29 21:16:38 +0800

    added default admin and default subject in TestUtils

--- src/testCommon/java/com/orangeandbronze/enlistment/domain/TestUtils.java
+++ src/testCommon/java/com/orangeandbronze/enlistment/domain/TestUtils.java
@@ -11,8 +11,13 @@ public class TestUtils {
     public static final Schedule TF830to10 = new Schedule(TF, new Period(LocalTime.of(8, 30), LocalTime.of(10, 0)));
     public static final Schedule TF10to1130 = new Schedule(TF, new Period(LocalTime.of(10, 0), LocalTime.of(11, 30)));
     public static final Subject DEFAULT_SUBJECT = new Subject("DefaultSubject");
+    public static final String DEFAULT_SUBJECT_ID = "DefaultSubject";
     public static final String DEFAULT_SECTION_ID = "DefaultSection";
     public static int DEFAULT_STUDENT_NUMBER = 10;
+
+
+    public static final int DEFAULT_ADMIN_ID = 20;
+
 
     public static Student newStudent(int studentNumber, Collection<Section> sections) {
         return new Student(studentNumber, "x", "x", sections);
