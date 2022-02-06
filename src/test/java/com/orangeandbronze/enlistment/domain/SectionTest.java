f75da46bfd206d817060482d9b509cb75a7cb18b
alenna <rainiiicorn@gmail.com>
2022-02-06 15:26:47 +0800

    added test for section with same faculty diff sched & same faculty conflict sched

--- src/test/java/com/orangeandbronze/enlistment/domain/SectionTest.java
+++ src/test/java/com/orangeandbronze/enlistment/domain/SectionTest.java
@@ -11,7 +11,7 @@ class SectionTest {
     void newSection_same_room_diff_sked() {
         Room room = new Room("X", 10);
         new Section("A", DEFAULT_SUBJECT, MTH830to10, room, newFaculty(1));
-        assertDoesNotThrow(() -> new Section("B", DEFAULT_SUBJECT, TF10to1130, room, newFaculty(2)));
+        assertDoesNotThrow(() -> new Section("B", DEFAULT_SUBJECT, TF10to1130, room, newFaculty(2 )));
     }
 
     @Test
@@ -20,5 +20,22 @@ class SectionTest {
         new Section("A", DEFAULT_SUBJECT, MTH830to10, room, newFaculty(1));
         assertThrows(ScheduleConflictException.class, () -> new Section("B", DEFAULT_SUBJECT, MTH830to10, room, newFaculty(2)));
     }
+
+
+    @Test
+    void newSection_same_faculty_diff_sched(){
+
+        Room room = new Room("X", 10);
+        new Section("A", DEFAULT_SUBJECT, MTH830to10, room, DEFAULT_FACULTY);
+        assertDoesNotThrow(() -> new Section("B", DEFAULT_SUBJECT, TF830to10, room, DEFAULT_FACULTY));
+    }
+
+    @Test
+    void newSection_same_faculty_conflict_sched(){
+        Room room = new Room("X", 10);
+        new Section("A", DEFAULT_SUBJECT, MTH830to10, room, DEFAULT_FACULTY);
+        assertThrows(ScheduleConflictException.class, () -> new Section("B", DEFAULT_SUBJECT, MTH9to11, room, DEFAULT_FACULTY));
+    }
+
 
 }
--- src/testCommon/java/com/orangeandbronze/enlistment/domain/TestUtils.java
+++ src/testCommon/java/com/orangeandbronze/enlistment/domain/TestUtils.java
@@ -7,15 +7,22 @@ import static com.orangeandbronze.enlistment.domain.Days.*;
 
 public class TestUtils {
 
+
     public static final Schedule MTH830to10 = new Schedule(MTH, new Period(LocalTime.of(8, 30), LocalTime.of(10, 0)));
     public static final Schedule TF830to10 = new Schedule(TF, new Period(LocalTime.of(8, 30), LocalTime.of(10, 0)));
     public static final Schedule TF10to1130 = new Schedule(TF, new Period(LocalTime.of(10, 0), LocalTime.of(11, 30)));
-    public static final Subject DEFAULT_SUBJECT = new Subject("DefaultSubject");
+    public static final Schedule MTH9to11 = new Schedule(MTH, new Period(LocalTime.of(9, 00), LocalTime.of(11, 0)));
+
     public static final String DEFAULT_SUBJECT_ID = "DefaultSubject";
+    public static final Subject DEFAULT_SUBJECT = new Subject(DEFAULT_SUBJECT_ID);
     public static final String DEFAULT_SECTION_ID = "DefaultSection";
+
     public static final int DEFAULT_STUDENT_NUMBER = 10;
-    public static final int DEFAULT_FACULTY_NUMBER = 1000;
-    public static Faculty DEFAULT_FACULTY = new Faculty(DEFAULT_FACULTY_NUMBER);
+    public static final int DEFAULT_FACULTY_NUMBER = 100;
+    public static final Faculty DEFAULT_FACULTY = new Faculty(DEFAULT_FACULTY_NUMBER);
+
+
+
 
     public static Faculty newFaculty(int facultyNumber){
         return new Faculty(facultyNumber);
