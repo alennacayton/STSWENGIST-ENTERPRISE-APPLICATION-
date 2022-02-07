4ef7927f3dc41bdb23fb420f1a2ec33b954c473a
akiescarraga <aki_escarraga@dlsu.edu.ph>
2022-02-07 15:23:10 +0800

    added SameSectionIdException

---
+++ src/main/java/com/orangeandbronze/enlistment/domain/SameSectionIdException.java
@@ -0,0 +1,7 @@
+package com.orangeandbronze.enlistment.domain;
+
+public class SameSectionIdException extends EnlistmentException {
+    public SameSectionIdException(String msg) {
+        super(msg);
+    }
+}
