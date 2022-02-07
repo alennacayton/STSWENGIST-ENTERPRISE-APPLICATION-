016d620e8b9e7a3829c44921983978bd93e6b97b
akiescarraga <aki_escarraga@dlsu.edu.ph>
2022-02-07 15:40:28 +0800

    updated test implementation for EnlistControllerTest

--- src/test/java/com/orangeandbronze/enlistment/controllers/EnlistControllerTest.java
+++ src/test/java/com/orangeandbronze/enlistment/controllers/EnlistControllerTest.java
@@ -1,8 +1,10 @@
 package com.orangeandbronze.enlistment.controllers;
 
 import com.orangeandbronze.enlistment.domain.*;
+import org.hibernate.Session;
 import org.junit.jupiter.api.*;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 import static com.orangeandbronze.enlistment.domain.TestUtils.*;
@@ -13,21 +15,26 @@ class EnlistControllerTest {
 
     @Test
     void enlistOrCancel_enlist_student_in_section() {
-        Student student = new Student(1, "X", "X");
-        String sectionId = "X";
+        Student student = mock(Student.class);
         UserAction userAction = UserAction.ENLIST;
 
         // when enlist (post) method is called
         SectionRepository sectionRepository = mock(SectionRepository.class);
         Section section = newDefaultSection();
-        when(sectionRepository.findById(sectionId)).thenReturn(Optional.of(section));
+        when(sectionRepository.findById(DEFAULT_SECTION_ID)).thenReturn(Optional.of(section));
         StudentRepository studentRepository = mock(StudentRepository.class);
         EnlistController controller = new EnlistController();
         controller.setSectionRepo(sectionRepository);
         controller.setStudentRepo(studentRepository);
-        String returnVal = controller.enlistOrCancel(student, sectionId, userAction);
+
+        EntityManager entityManager = mock(EntityManager.class);
+        Session session = mock(Session.class);
+        when(entityManager.unwrap(Session.class)).thenReturn(session);
+        controller.setEntityManager(entityManager);
+
+        String returnVal = controller.enlistOrCancel(student, DEFAULT_SECTION_ID, userAction);
         // Retrieve the Section object from the DB using sectionId
-        verify(sectionRepository).findById(sectionId);
+        verify(sectionRepository).findById(DEFAULT_SECTION_ID);
         // student.enlist method will be called, passing in the section
         verify(student).enlist(section);
         // save student to DB
